6d15d729a8d66201bfe162a4076edef17fb27dcf
David Marchante <cdmarchante@gmail.com>
2019-06-04 17:32:41 -0700

    fast forward

--- 401/challenges/whiteboardChallenges/src/test/java/whiteboardChallenges/linkedList/LinkedListTest.java
+++ 401/challenges/whiteboardChallenges/src/test/java/whiteboardChallenges/linkedList/LinkedListTest.java
@@ -1,5 +1,6 @@
 package whiteboardChallenges.linkedList;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -8,13 +9,19 @@ import java.util.Arrays;
 import static org.junit.Assert.*;
 
 public class LinkedListTest {
-    @Test
-    public void testLinkList_append() {
-        LinkedList libraryForTesting = new LinkedList();
+    LinkedList libraryForTesting = new LinkedList();
+
+    @Before
+    public void setUp () {
+        libraryForTesting = new LinkedList();
         libraryForTesting.insert(3);
         libraryForTesting.insert(6);
         libraryForTesting.insert(8);
         libraryForTesting.insert(9);
+    }
+
+    @Test
+    public void testLinkList_append() {
         libraryForTesting.append(1);
 
         ArrayList<Integer> expectedList = new ArrayList<>(Arrays.asList(9, 8, 6, 3, 1));
@@ -25,11 +32,6 @@ public class LinkedListTest {
 
     @Test
     public void testLinkList_appendMultiple() {
-        LinkedList libraryForTesting = new LinkedList();
-        libraryForTesting.insert(3);
-        libraryForTesting.insert(6);
-        libraryForTesting.insert(8);
-        libraryForTesting.insert(9);
         libraryForTesting.append(1);
         libraryForTesting.append(2);
 
@@ -41,11 +43,6 @@ public class LinkedListTest {
 
     @Test
     public void testLinkList_insertBefore() {
-        LinkedList libraryForTesting = new LinkedList();
-        libraryForTesting.insert(3);
-        libraryForTesting.insert(6);
-        libraryForTesting.insert(8);
-        libraryForTesting.insert(9);
         libraryForTesting.insertBefore(6, 1);
 
         ArrayList<Integer> expectedList = new ArrayList<>(Arrays.asList(9, 8, 1, 6, 3));
@@ -56,11 +53,6 @@ public class LinkedListTest {
 
     @Test
     public void testLinkList_insertBeforeFirst() {
-        LinkedList libraryForTesting = new LinkedList();
-        libraryForTesting.insert(3);
-        libraryForTesting.insert(6);
-        libraryForTesting.insert(8);
-        libraryForTesting.insert(9);
         libraryForTesting.insertBefore(9, 1);
 
         ArrayList<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 9, 8, 6, 3));
@@ -77,11 +69,6 @@ public class LinkedListTest {
 
     @Test
     public void testLinkList_insertAfter() {
-        LinkedList libraryForTesting = new LinkedList();
-        libraryForTesting.insert(3);
-        libraryForTesting.insert(6);
-        libraryForTesting.insert(8);
-        libraryForTesting.insert(9);
         libraryForTesting.insertAfter(6, 1);
 
         ArrayList<Integer> expectedList = new ArrayList<>(Arrays.asList(9, 8, 6, 1, 3));
@@ -92,11 +79,6 @@ public class LinkedListTest {
 
     @Test
     public void testLinkList_insertAfterLast() {
-        LinkedList libraryForTesting = new LinkedList();
-        libraryForTesting.insert(3);
-        libraryForTesting.insert(6);
-        libraryForTesting.insert(8);
-        libraryForTesting.insert(9);
         libraryForTesting.insertAfter(3, 1);
 
         ArrayList<Integer> expectedList = new ArrayList<>(Arrays.asList(9, 8, 6, 3, 1));
@@ -113,12 +95,6 @@ public class LinkedListTest {
 
     @Test
     public void testLinkList_insertFalse() {
-        LinkedList libraryForTesting = new LinkedList();
-        libraryForTesting.insert(3);
-        libraryForTesting.insert(6);
-        libraryForTesting.insert(8);
-        libraryForTesting.insert(9);
-
         boolean testBoolean = libraryForTesting.includes(4);
 
         assertFalse(testBoolean);
@@ -126,12 +102,6 @@ public class LinkedListTest {
 
     @Test
     public void testLinkList_insertTrue() {
-        LinkedList libraryForTesting = new LinkedList();
-        libraryForTesting.insert(3);
-        libraryForTesting.insert(6);
-        libraryForTesting.insert(8);
-        libraryForTesting.insert(9);
-
         boolean testBoolean = libraryForTesting.includes(3);
 
         assertTrue(testBoolean);
@@ -139,12 +109,6 @@ public class LinkedListTest {
 
     @Test
     public void testLinkList_print() {
-        LinkedList libraryForTesting = new LinkedList();
-        libraryForTesting.insert(3);
-        libraryForTesting.insert(6);
-        libraryForTesting.insert(8);
-        libraryForTesting.insert(9);
-
         ArrayList<Integer> expectedList = new ArrayList<>(Arrays.asList(9, 8, 6, 3));
         ArrayList<Integer> testList = libraryForTesting.printLinkedList();
 
