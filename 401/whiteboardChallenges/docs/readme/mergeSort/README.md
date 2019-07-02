0e2b836879d3a4367f8fc8e377f040fc07209a00
David Marchante <cdmarchante@gmail.com>
2019-07-02 09:58:14 -0700

    complete merge sort assignment

---
+++ 401/whiteboardChallenges/docs/readme/mergeSort/README.md
@@ -0,0 +1,11 @@
+# Challenge Name
+Merge Sort
+
+## Challenge Description
+Given psuedo code of insermergeion sort, write code in Java with Test and develop Lesson Plan.
+
+## Approach & Efficiency
+Psuedocode was provided. Time: O(n), Space: O(1)
+
+## Solution
+![Insertion Sort Whiteboard](../../assets/mergeSort-whiteboard.jpg)
---
+++ 401/whiteboardChallenges/src/test/java/whiteboardChallenges/sort/SortTest.java
@@ -0,0 +1,119 @@
+package whiteboardChallenges.sort;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class SortTest {
+    private Sort sortClass = new Sort();
+    List<Integer> arr = new ArrayList<>();
+    List<Integer> arrCheck = new ArrayList<>();
+
+    @Test
+    public void testMergeSort() {
+        this.arr.add(9);
+        this.arr.add(7);
+        this.arr.add(6);
+        this.arr.add(5);
+        this.arr.add(8);
+        this.arr.add(4);
+
+        this.arrCheck.add(4);
+        this.arrCheck.add(5);
+        this.arrCheck.add(6);
+        this.arrCheck.add(7);
+        this.arrCheck.add(8);
+        this.arrCheck.add(9);
+
+        List<Integer> arrayActual = this.sortClass.mergeSort(arr);
+
+        assertEquals(this.arrCheck, arrayActual);
+    }
+
+    @Test
+    public void testMergeSort_sameValues() {
+        this.arr.add(5);
+        this.arr.add(5);
+        this.arr.add(5);
+        this.arr.add(5);
+
+        this.arrCheck.add(5);
+        this.arrCheck.add(5);
+        this.arrCheck.add(5);
+        this.arrCheck.add(5);
+
+        List<Integer> arrayActual = this.sortClass.mergeSort(arr);
+
+        assertEquals(this.arrCheck, arrayActual);
+    }
+
+    @Test
+    public void testMergeSort_one() {
+        this.arr.add(5);
+        this.arrCheck.add(5);
+
+        List<Integer> arrayActual = this.sortClass.mergeSort(arr);
+
+        assertEquals(this.arrCheck, arrayActual);
+    }
+
+    @Test
+    public void testMergeSort_empty() {
+        List<Integer> arrayActual = this.sortClass.mergeSort(arr);
+
+        assertEquals(this.arr, arrayActual);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testMergeSort_null() {
+        this.arr = null;
+        List<Integer> arrayActual = this.sortClass.mergeSort(arr);
+
+        assertEquals(this.arr, arrayActual);
+    }
+
+    @Test
+    public void testInsertionSort() {
+        int[] arr = new int[] {3, 1, 7, 8, 5};
+        int[] arrCheck = new int[] {1, 3, 5, 7, 8};
+        int[] arrayActual = this.sortClass.insertionSort(arr);
+
+        assertArrayEquals(arrCheck, arrayActual);
+    }
+
+    @Test
+    public void testInsertionSort_sameValues() {
+        int[] arrSameValues = new int[] {2, 2, 2, 2, };
+        int[] arrayActual = this.sortClass.insertionSort(arrSameValues);
+
+        assertArrayEquals(arrSameValues, arrayActual);
+    }
+
+    @Test
+    public void testInsertionSort_one() {
+        int[] arrOneValue = new int[] {3};
+        int[] arrayActual = this.sortClass.insertionSort(arrOneValue);
+
+        assertArrayEquals(arrOneValue, arrayActual);
+    }
+
+    @Test
+    public void testInsertionSort_empty() {
+        int[] arrEmpty = new int[] {};
+        int[] arrayActual = this.sortClass.insertionSort(arrEmpty);
+
+        assertArrayEquals(arrEmpty, arrayActual);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testInsertionSort_null() {
+        int[] arrNull = null;
+        int[] arrayActual = this.sortClass.insertionSort(arrNull);
+
+        assertArrayEquals(arrNull, arrayActual);
+    }
+}
